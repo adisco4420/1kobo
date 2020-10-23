b21eba303c4e0c0fc4c8fbf4124fcd7acbc8e2c6
Alabi Sodiq <adisco4420@gmail.com>
2020-10-23 20:19:27 +0100

    login page design

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,17 @@
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent,
+    children: [
+      {path: 'login', component: LoginComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, AuthLayoutComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
