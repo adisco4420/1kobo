d94fae3536c7724f629a93d6554d27d381c67021
Alabi Sodiq <adisco4420@gmail.com>
2020-11-28 10:00:20 +0100

    registration api inti getting started

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,13 +6,16 @@ import { LoginComponent } from './login/login.component';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent, AuthLayoutComponent, RegisterComponent, ForgotPwdComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
