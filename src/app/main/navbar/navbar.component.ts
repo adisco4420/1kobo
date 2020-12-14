26f7b9b54d2a240cf949e5c292adf4a2357d073e
Sodiq Alabi <adisco4420@gmail.com>
2020-12-14 16:24:33 +0100

    navbar components

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authSrv: AuthService) { }
 
   ngOnInit(): void {
+  }
+  get isLoggedIn() {
+    return this.authSrv.isUserLoggedIn;
   }
 
 }
